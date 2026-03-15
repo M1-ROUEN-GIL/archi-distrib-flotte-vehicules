3b69e97daca2c1ed460c0cdd000a09cbb7f2a508
Ahcene Amouchas <ahcene.amouchas@univ-rouen.fr>
2026-03-16 00:53:43 +0100

    ajout des service + dockerfile

---
+++ services/maintenance-service/src/test/java/com/flotte/maintenance/MaintenanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flotte.maintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ services/vehicle-service/src/test/java/com/flotte/vehicle/VehicleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flotte.vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
